29a2fb5efa1e59a3490f98bc130d476f36aa0b22
connerharwood <33815089+connerharwood@users.noreply.github.com>
2024-04-12 15:22:31 -0600

    Find straddling counties, calculate number of cities lying outside basin within each county, calculate percent of each county lying within basin

--- code/straddling_counties.R
+++ code/straddling_counties.R
@@ -1,28 +1,17 @@
 library(tidyverse)
 library(sf)
-library(tmap)
 
 #------------------------------------------------------------------------------#
-# load data ----
 
-# load Wisconsin counties shapefile
+# load shapefiles
 all_counties = st_read("~/562-Project/raw-data/counties-shapefile/wisconsin_counties.shp")
-
-# load Wisconsin cities shapefile
 all_cities = st_read("~/562-Project/raw-data/cities-shapefile/wisconsin_cities.shp")
-
-# load Great Lakes Basin shapefile
 basin = st_read("~/562-Project/raw-data/basin-shapefile/subbasins.shp")
 
-# make valid
-all_counties = st_make_valid(all_counties)
-all_cities = st_make_valid(all_cities)
-basin = st_make_valid(basin)
-
-# transform CRS of shapefiles to WGS 84
-all_counties = st_transform(all_counties, crs = 4326)
-all_cities = st_transform(all_cities, crs = 4326)
-basin = st_transform(basin, crs = 4326)
+# make valid and transform CRS to WGS 84
+all_counties = all_counties |> st_make_valid() |> st_transform(crs = 4326)
+all_cities = all_cities |> st_make_valid() |> st_transform(crs = 4326)
+basin = basin |> st_make_valid() |> st_transform(crs = 4326)
 
 #------------------------------------------------------------------------------#
 # find straddling counties ----
@@ -42,12 +31,18 @@ fully_within_counties = all_counties[st_within(all_counties, subbasins, sparse =
 straddling_counties_partial = basin_counties |> 
   filter(!COUNTY_NAM %in% fully_within_counties$COUNTY_NAM)
 
-#------------------------------------------------------------------------------#
-# percent of each county lying within basin ----
+# grab subbasin of each county for later
+county_subbasin = straddling_counties_partial |> 
+  select(county = COUNTY_NAM, subbasin) |>
+  mutate(county = paste(county, "County")) |> 
+  st_drop_geometry()
 
 # full polygons of straddling counties
 straddling_counties_full = all_counties |> 
   filter(COUNTY_NAM %in% straddling_counties_partial$COUNTY_NAM)
+
+#------------------------------------------------------------------------------#
+# percent of each county lying within basin ----
 
 # calculate full area of each county
 straddling_counties_full$area = st_area(straddling_counties_full)
@@ -56,7 +51,7 @@ straddling_counties_full$area = st_area(straddling_counties_full)
 straddling_counties_partial$area = st_area(straddling_counties_partial)
 
 # sum up each county's partial area (some counties have multiple geometries)
-straddling_counties_partial2 = straddling_counties_partial |> 
+straddling_counties_partial = straddling_counties_partial |> 
   group_by(COUNTY_NAM) |> 
   summarize(area = sum(area)) |> 
   # sort by county name
@@ -66,7 +61,7 @@ straddling_counties = straddling_counties_full |>
   # sort by county name
   arrange(COUNTY_NAM) |> 
   # calculate percent of each county lying within Great Lakes Basin
-  mutate(percent_within = straddling_counties_partial2$area / area) |> 
+  mutate(percent_within = straddling_counties_partial$area / area) |> 
   # select and rename relevant variables
   select(
     county = COUNTY_NAM,
@@ -81,36 +76,50 @@ straddling_counties = straddling_counties_full |>
   )
 
 #------------------------------------------------------------------------------#
-# number of cities contained in each straddling county ----
+# number of cities in each straddling county lying fully outside the basin ----
 
 # filter cities data to only include straddling counties
 straddling_cities = all_cities |>
-  # add "County" to each county
-  mutate(CNTY_NAME = paste(CNTY_NAME, "County")) |> 
-  filter(CNTY_NAME %in% straddling_counties$county)
-
-# calculate number of cities in each straddling county
-cities_per_county = straddling_cities |> 
-  select(
-    county = CNTY_NAME,
-    city = MCD_NAME
+  rename(county = CNTY_NAME, city = MCD_NAME) |> 
+  mutate(
+    # add "County" to each county
+    county = paste(county, "County"),
+    # convert city to title case
+    city = str_to_title(city)
   ) |> 
+  filter(county %in% straddling_counties$county)
+
+# cities lying at least partially within Great Lakes Basin
+cities_within = st_intersection(straddling_cities, subbasins)
+
+# cities lying fully outside the basin, but within a straddling county
+cities_outside = straddling_cities |> 
+  filter(!city %in% cities_within$city)
+
+# calculate number of cities lying fully outside basin in each straddling county
+cities_per_county = cities_outside |> 
   # convert city names to title case
-  mutate(city = str_to_title(city)) |> 
   distinct(county, city) |> 
   group_by(county) |> 
-  summarize(n_cities = n())
+  summarize(n_cities_outside = n())
 
 # merge with straddling counties sf object
 straddling_counties = left_join(straddling_counties, cities_per_county, by = "county")
+  
+# replace NAs with 0
+straddling_counties = straddling_counties |> 
+  mutate(n_cities_outside = ifelse(is.na(n_cities_outside), 0, n_cities_outside))
 
 #------------------------------------------------------------------------------#
-# distance to nearest Great Lake ----
 
+# merge each county's subbasin
+straddling_counties = left_join(straddling_counties, county_subbasin, by = "county")
 
+# drop Vilas County observation with Lake Michigan subbasin (mostly irrelevant)
+straddling_counties = straddling_counties |> 
+  filter(!(county == "Vilas County" & subbasin == "lk_mich"))
 
 #------------------------------------------------------------------------------#
-
 st_write(
   straddling_counties, 
   "~/OneDrive - Montana State University/Metrics Project/wisconsin/data/straddling counties shapefile/straddling_counties.shp",
