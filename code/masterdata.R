408a1b02d3135fe18329a75f82d115c8f2f1290a
connerharwood <33815089+connerharwood@users.noreply.github.com>
2024-04-12 23:48:31 -0600

    .

--- code/masterdata.R
+++ code/masterdata.R
@@ -2,4 +2,7 @@ library(tidyverse)
 library(sf)
 
 straddling_counties = st_read("~/562-Project/clean-data/straddling-counties/straddling_counties.shp")
-fred1997_2022 = 
+fred1997_2022 = readRDS("~/562-Project/clean-data/fred.rds")
+
+# merge
+masterdata_raw = left_join(fred1997_2022, straddling_counties, by = "county", relationship = "many-to-one")
