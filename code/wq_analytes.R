6d8fdedebc66390131e43aa33b2e0ec835dfb25c
connerharwood <33815089+connerharwood@users.noreply.github.com>
2024-04-16 13:32:54 -0600

    Delete unused water quality data Clean water quality data

--- code/wq_analytes.R
+++ code/wq_analytes.R
@@ -11,28 +11,6 @@ wq5_raw = read_xlsx("~/562-Project/raw-data/wells/wq5.xlsx")
 # append into one dataset
 wq_raw = rbind(wq1_raw, wq2_raw, wq3_raw, wq4_raw, wq5_raw)
 
-
-
-# raw well water quality data for different contaminant types
-bacteria_raw = read_xlsx("~/562-Project/raw-data/wells/bacteria.xlsx")
-nitrate1_raw = read_xlsx("~/562-Project/raw-data/wells/nitrate1.xlsx")
-nitrate2_raw = read_xlsx("~/562-Project/raw-data/wells/nitrate2.xlsx")
-pesticides_raw = read_xlsx("~/562-Project/raw-data/wells/pesticides.xlsx")
-voc1_raw = read_xlsx("~/562-Project/raw-data/wells/voc1.xlsx")
-voc2_raw = read_xlsx("~/562-Project/raw-data/wells/voc2.xlsx")
-other_raw = read_xlsx("~/562-Project/raw-data/wells/other.xlsx")
-
-# append into one dataset
-wq_raw = rbind(
-  bacteria_raw,
-  nitrate1_raw,
-  nitrate2_raw,
-  pesticides_raw,
-  voc1_raw,
-  voc2_raw,
-  other_raw
-)
-
 #------------------------------------------------------------------------------#
 # cleaning ----
 
@@ -63,10 +41,102 @@ wq1 = wq_raw |>
     sample_qualifier = str_to_sentence(sample_qualifier)
   ) |> 
   # remove NAs
-  filter(!is.na(sample_result) & !is.na(unit))
+  filter(!is.na(sample_result) & !is.na(unit) &!is.na(param))
 
 # parameters with samples in all years for all counties
 year_counts = wq1 |> 
   group_by(param, param_code) |> 
   summarize(n = n_distinct(year)) |> 
   filter(n == 26)
+
+# filter to include only potentially relevant parameters
+wq2 = wq1 |> 
+  filter(
+    param_code %in% c(
+      "410", "1002", "39033", "940", "99060", "95", "1042", "720", "951", "39941", "900", "74010", "1045", "1051", "1055",
+      "71900", "618", "620", "39516", "403", "1501", "3501", "82303", "1147", "945", "1092", "11503"
+    )
+  )
+
+# check how much each measurement unit for each sample parameter is used
+units = wq2 |> 
+  group_by(param, param_code, unit) |> 
+  summarize(unit_count = n()) |> 
+  arrange(param)
+
+# filter out observations with inconsistent units better left dropped rather than converted
+wq3 = wq2 |> 
+  filter(
+    !(param_code == "95" & unit == "fib/l") &
+    !(param_code == "95" & unit == "mg/l") &
+    !(param_code == "95" & unit == "ug/l") &
+    !(param_code == "95" & unit == "units") &
+    !(param_code == "403" & sample_result == 0)
+  )
+
+wq4 = wq3 |> 
+  mutate(
+    # convert between ug/l and mg/l for certain parameters
+    sample_result = case_when(
+      param_code == "1002" & unit == "mg/l" ~ sample_result*1000,
+      param_code == "1042" & unit == "mg/l" ~ sample_result*1000,
+      param_code == "720" & unit == "ug/l" ~ sample_result/1000,
+      param_code == "900" & unit == "ug/l" ~ sample_result/1000,
+      param_code == "74010" & unit == "ug/l" ~ sample_result/1000,
+      param_code == "1045" & unit == "ug/l" ~ sample_result/1000,
+      param_code == "1051" & unit == "mg/l" ~ sample_result*1000,
+      param_code == "1055" & unit == "mg/l" ~ sample_result*1000,
+      param_code == "71900" & unit == "ug/l" ~ sample_result/1000,
+      param_code == "618" & unit == "ug/l" ~ sample_result/1000,
+      param_code == "620" & unit == "ug/l" ~ sample_result/1000,
+      param_code == "1147" & unit == "ug/l" ~ sample_result/1000,
+      param_code == "1092" & unit == "ug/l" ~ sample_result/1000,
+      TRUE ~ sample_result
+    ),
+    # align units for parameters
+    unit = case_when(
+      param_code == "1002" & unit == "mg/l" ~ "ug/l",
+      param_code == "99060" & sample_result == 0 ~ "absent",
+      param_code == "99060" & sample_result == 1 ~ "present",
+      param_code == "95" & unit == "umhos/cm" ~ "umho/cm",
+      param_code == "95" & unit == "us/cm" ~ "umho/cm",
+      param_code == "1042" & unit == "mg/l" ~ "ug/l",
+      param_code == "720" & unit == "ug/l" ~ "mg/l",
+      param_code == "900" & unit == "ug/l" ~ "mg/l",
+      param_code == "74010" & unit == "ug/l" ~ "mg/l",
+      param_code == "1045" & unit == "ug/l" ~ "mg/l",
+      param_code == "1051" & unit == "mg/l" ~ "ug/l",
+      param_code == "1055" & unit == "mg/l" ~ "ug/l",
+      param_code == "71900" & unit == "ug/l" ~ "mg/l",
+      param_code == "618" & unit == "ug/l" ~ "mg/l",
+      param_code == "620" & unit == "ug/l" ~ "mg/l",
+      param_code == "620" & unit == "mg/l as n" ~ "mg/l",
+      param_code == "403" ~ "su",
+      param_code == "1147" & unit == "ug/l" ~ "mg/l",
+      param_code == "1092" & unit == "ug/l" ~ "mg/l",
+      TRUE ~ unit
+    ),
+    # change parameter names
+    param = case_when(
+      param_code == "410" ~ "Alkalinity total CaCO3",
+      param_code == "99060" ~ "Coliform total Colilert absent/present",
+      param_code == "39941" ~ "Glyphosate (Roundup)",
+      param_code == "900" ~ "Hardness total CaCO3",
+      param_code == "618" ~ "Nitrogen NO3-N diss",
+      param_code == "620" ~ "Nitrogen NO3-N total",
+      param_code == "39516" ~ "PCB total",
+      param_code == "403" ~ "pH",
+      TRUE ~ param
+    )
+  )
+
+# check units again
+units = wq4 |> 
+  group_by(param, param_code, unit) |> 
+  summarize(unit_count = n()) |> 
+  arrange(param)
+
+# most relevant parameters for Wisconsin: coliform (bacteria), nitrate (NO3), arsenic, copper, lead, radium
+wq5 = wq4 |> 
+  filter(param_code %in% c("99060", "620", "1002", "1042", "1051", "11503"))
+#------------------------------------------------------------------------------#
