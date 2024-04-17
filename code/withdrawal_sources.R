6ab3f81e79712425469a055a446d3596049a9c39
connerharwood <33815089+connerharwood@users.noreply.github.com>
2024-04-16 19:11:24 -0600

    Add new water source data

--- code/withdrawal_sources.R
+++ code/withdrawal_sources.R
@@ -1,18 +1,35 @@
 library(tidyverse)
+library(lubridate)
 
 sources_raw = read_csv("~/562-Project/raw-data/withdrawal_sources.csv")
 
 #------------------------------------------------------------------------------#
 
 sources = sources_raw |> 
-  select(county, hicap_num, water_use, approval_date, construction_date, well_num, status, source, max_approved_gpm) |> 
+  # select and rename relevant variables
+  select(
+    county, 
+    hicap_num, 
+    water_use, 
+    approval_year = approval_date, 
+    construction_year = construction_date, 
+    abandoned_year, 
+    well_num, 
+    status, 
+    source, 
+    max_approved_gpm
+  ) |> 
   mutate(
     # add "County" to each county entry
     county = paste0(county, " County"),
     # convert date variables to date format
-    approval_date = as.Date(approval_date, format = "%m/%d/%y"),
-    construction_date = as.Date(construction_date, format = "%m/%d/%y"),
+    approval_year = as.Date(approval_year, format = "%m/%d/%y"),
+    construction_year = as.Date(construction_year, format = "%m/%d/%y"),
     # extract year, convert to numeric
-    approval_year = year(approval_date),
-    construction_year = year(construction_date)
+    approval_year = year(approval_year),
+    construction_year = year(construction_year),
+    # subtract 100 from years past 2024
+    approval_year = ifelse(approval_year > 2024, approval_year - 100, approval_year),
+    construction_year = ifelse(construction_year > 2024, construction_year - 100, construction_year)
   )
+
