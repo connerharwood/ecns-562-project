45fcf7253b6df60d9873241ce440090e5485a573
connerharwood <33815089+connerharwood@users.noreply.github.com>
2024-04-28 15:55:35 -0600

    Save cleaned radium data as .rds file

--- code/well_quality.R
+++ code/well_quality.R
@@ -1,99 +1,90 @@
 library(tidyverse)
 
-# Wisconsin drinking water quality data
+# Wisconsin drinking water radium data
 radium_raw = read_csv("~/562-Project/raw-data/water-quality/radium.csv")
-
-wq_raw_raw = rbind(radium_raw, )
 
 #------------------------------------------------------------------------------#
 # cleaning ----
 
 radium = radium_raw |> 
   select(
+    # select and rename relevant variables
     county = County,
     year = `Sample Date`,
     contaminant = Contaminant,
     measurement = `Measured Amount`,
     units = Units,
-    id = `Sample ID`,
-    storet_code = `Storet Code`
-  ) |> 
-  mutate(
-    county = paste(county, "County"),
-    year = as.numeric(substr(year, 7, 10))
-  )
-  
-yearcounty_ave = radium |> 
-  group_by(county, year) |> 
-  summarize(average = mean(measurement))
-  
-radium_226_228 = radium |> 
-  
-  
-  
-  
-  
-check = radium |> 
-  group_by(year, id) |> 
-  summarize(count = n_distinct(contaminant))
-  
-
-
-well_quality1 = well_quality_raw |> 
-  # select and rename relevant variables
-  select(
-    well_id = `WI Unique Well #`,
-    county = County,
-    well_use = `Well Use`,
-    well_status = `Well Status`,
-    sample_id = `Labslip # / Sample ID`,
-    sample_date = `Sample Collection Date`,
-    param_description = `Storet Parameter Description`,
-    param_code = `Storet Parameter Code`,
-    sample_result = `Sample Analytical Result Amount`,
-    unit = Units,
-    sample_qualifier = `Sample Analytical Qualifier`,
-    standard = `Enforcement Standard`,
-    limit = `Preventive Action Limit`,
+    id = `Sample ID`
   ) |> 
   mutate(
     # add "County" to each county name
     county = paste(county, "County"),
-    well_use = str_to_sentence(well_use),
-    well_status = str_to_sentence(well_status),
     # extract year from date, convert to numeric
-    sample_date = as.numeric(substr(sample_date, 1, 4)),
-    param_description = str_to_sentence(param_description),
-    unit = str_to_lower(unit),
-    sample_qualifier = str_to_sentence(sample_qualifier)
+    year = as.numeric(substr(year, 7, 10))
   )
 
-# check how much each measurement unit for each sample parameter is used
-units = well_quality2 |> 
-  group_by(param_description, unit) |> 
-  summarize(unit_count = n()) |> 
-  arrange(param_description)
-
-# additional cleaning
-well_quality2 = well_quality1 |> 
-  # remove NAs
-  filter(
-    !is.na(unit) & 
-    !is.na(sample_result) & 
-    !(param_description == "Arsenic total" & unit == "ug/l") &
-    !(param_description == "Copper total" & unit == "mg/l") &
-    !(param_description == "Hardness total caco3" & unit == "ug/l") &
-    !(param_description == "Iron total" & unit == "ug/l") &
-    !(param_description == "Lead" & unit == "mg/l") &
-    !(param_description == "Manganese" & unit == "mg/l") &
-    !(param_description == "Ph field" & unit == "units") &
-    !(param_description == "Phosphorus total" & unit == "ug/l")
+# calculate average measurement of each contaminant for each sample ID
+radium2 = radium |> 
+  group_by(id, contaminant, county, year, units) |> 
+  summarize(
+    measurement = mean(measurement)
   )
 
-yearly = well_quality2 |> 
-  group_by(county, sample_date, param_description) |> 
-  summarize(n = n())
+# measurements of Radium 226+228
+radium_226_228 = radium2 |> 
+  filter(contaminant == "RADIUM, (226 + 228)") |> 
+  select(id, contaminant, county, year, measurement, units)
 
-year_counts = well_quality2 |> 
-  group_by(param_description, param_code) |> 
-  summarize(n_years = n_distinct(sample_date))
+# sum Radium 226 + Radium 228 for samples without Radium 226+228 entry
+radium_summed = radium2 |> 
+  # sample IDs that don't have an entry for Radium 226+228
+  filter(!id %in% radium_226_228$id) |> 
+  group_by(id, county, year, units) |> 
+  summarize(
+    measurement = sum(measurement)
+  ) |> 
+  # add contaminant variable
+  mutate(contaminant = "RADIUM, (226 + 228)") |> 
+  select(id, contaminant, county, year, measurement, units)
+  
+# append Radium 226+228 data
+radium3 = rbind(radium_226_228, radium_summed)
+
+# county-year radium averages
+radium4 = radium3 |> 
+  group_by(county, year) |> 
+  summarize(ra_average = mean(measurement))
+
+#------------------------------------------------------------------------------#
+# impute missing years to be minimum average value for each county ----
+
+# grid of all year-county combinations
+grid = expand_grid(
+  county = unique(radium4$county),
+  year = unique(radium4$year)
+)
+
+# minimum average radium value for each county
+county_minimum = radium4 |> 
+  group_by(county) |> 
+  summarize(ra_minimum = min(ra_average, na.rm = TRUE)) |> 
+  # change Waushara's negative value to zero
+  mutate(
+    ra_minimum = case_when(
+      county == "Waushara County" ~ 0,
+      TRUE ~ ra_minimum
+    )
+  )
+
+# fill in missing values with respective county minimum
+impute = grid |> 
+  left_join(radium4, by = c("county", "year")) |> 
+  left_join(county_minimum, by = c("county")) |> 
+  mutate(ra_average = ifelse(is.na(ra_average), ra_minimum, ra_average))
+
+radium = impute
+
+#------------------------------------------------------------------------------#
+
+# save cleaned radium data
+saveRDS(radium, file = "~/562-Project/clean-data/radium.rds")
