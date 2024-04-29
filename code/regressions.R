85b19ec414a2460f33c33a659b03efe300ae2535
connerharwood <33815089+connerharwood@users.noreply.github.com>
2024-04-28 21:59:56 -0600

    Run some Firth regressions, get world's largest confidence intervals

--- code/regressions.R
+++ code/regressions.R
@@ -6,16 +6,24 @@ masterdata = readRDS("~/562-Project/clean-data/masterdata.rds")
 #------------------------------------------------------------------------------#
 
 firth = logistf(
-  approval ~ population + income + u_rate + permits + establishments + percent_within + n_cities_outside + sources_per_mi2,
-  data = masterdata_raw,
-  control = logistf.control(maxit = 1000),
-  flic = TRUE
+  approval ~ population + permits + establishments + percent_within + sources + radium + year,
+  data = masterdata
 )
 summary(firth)
 
-lk_mi_data = masterdata_raw |> 
-  filter(subbasin != "lk_sup") |> 
-  filter(year > 2000)
+firth = logistf(
+  approval ~ population + percent_within + permits + establishments + radium,
+  data = masterdata,
+  alpha = 0.90
+)
+summary(firth)
+
+
+firth = logistf(
+  approval ~ population + income + unemp + permits + establishments + percent_within + sources + radium,
+  data = masterdata
+)
+summary(firth)
 
 firth2 = logistf(
   approval ~ population + income + u_rate + percent_within + n_cities_outside + sources_per_mi2,
