41dc1f0d8255dae33145388b3e2b41248522dda9
connerharwood <33815089+connerharwood@users.noreply.github.com>
2024-04-30 17:33:27 -0600

    Update masterdata

--- code/radium.R
+++ code/radium.R
@@ -21,7 +21,9 @@ radium = radium_raw |>
     county = paste(county, "County"),
     # extract year from date, convert to numeric
     year = as.numeric(substr(year, 7, 10))
-  )
+  ) |> 
+  # filter to only include detects
+  filter(measurement > 0)
 
 # calculate average measurement of each contaminant for each sample ID
 radium2 = radium |> 
@@ -56,7 +58,7 @@ radium4 = radium3 |>
   summarize(ra_average = mean(measurement))
 
 #------------------------------------------------------------------------------#
-# impute missing years to be minimum average value for each county ----
+# impute missing years to be zero ----
 
 # grid of all year-county combinations
 grid = expand_grid(
@@ -64,25 +66,12 @@ grid = expand_grid(
   year = unique(radium4$year)
 )
 
-# minimum average radium value for each county
-county_minimum = radium4 |> 
-  group_by(county) |> 
-  summarize(ra_minimum = min(ra_average, na.rm = TRUE)) |> 
-  # change Waushara's negative value to zero
-  mutate(
-    ra_minimum = case_when(
-      county == "Waushara County" ~ 0,
-      TRUE ~ ra_minimum
-    )
-  )
-
-# fill in missing values with respective county minimum
-impute = grid |> 
+radium = grid |> 
   left_join(radium4, by = c("county", "year")) |> 
-  left_join(county_minimum, by = c("county")) |> 
-  mutate(ra_average = ifelse(is.na(ra_average), ra_minimum, ra_average))
-
-radium = impute |> select(-ra_minimum)
+  # replace NAs with zero
+  mutate(
+    ra_average = ifelse(is.na(ra_average), 0, ra_average)
+  )
 
 #------------------------------------------------------------------------------#
 
