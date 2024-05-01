b9310d07c5245ee017672e0b1e56e8608e59d878
connerharwood <33815089+connerharwood@users.noreply.github.com>
2024-05-01 13:54:03 -0600

    .

--- code/population_cleaning.R
+++ code/population_cleaning.R
@@ -1,12 +1,16 @@
 library(tidyverse)
 library(readxl)
+library(sf)
 
 pop2000_2010_raw = read_csv("~/562-Project/raw-data/city-populations/population2000_2010.csv")
 pop2010_2020_raw = read_csv("~/562-Project/raw-data/city-populations/population2010_2020.csv")
 pop2020_2022_raw = read_xlsx("~/562-Project/raw-data/city-populations/population2020_2022.xlsx")
 
+straddling_cities <- readRDS("~/562-Project/clean-data/straddling_cities.rds") |> st_drop_geometry()
+
 #------------------------------------------------------------------------------#
 # 2000-2010 ----
 
 pop1_raw = pop2000_2010_raw |> 
-  filter(STATE == 55)
+  filter(STATE == 55) |> 
+  filter(PLACE %in% straddling_cities$fips)
