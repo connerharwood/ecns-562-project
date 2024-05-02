0f5eb12226803b1b09715a437b8fda0b491d6087
connerharwood <33815089+connerharwood@users.noreply.github.com>
2024-05-02 01:48:21 -0600

    Plots

--- code/summary_stats.R
+++ code/summary_stats.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(sf)
-library("xtable")
+library(xtable)
 
 masterdata = readRDS("~/562-Project/clean-data/masterdata.rds")
 masterdata_changes = readRDS("~/562-Project/clean-data/masterdata_subset.rds")
@@ -54,3 +54,96 @@ changes_summary = masterdata_changes |>
 latex_code = xtable(changes_summary, caption = "1997-2022 Summary Statistics", digits = 3)
 
 print(latex_code)
+
+#------------------------------------------------------------------------------#
+# kenosha ----
+
+kenosha = masterdata_changes |> 
+  filter(county == "Kenosha County") |> 
+  mutate(
+    pop = population*100,
+    permits = permits*100,
+    establishments = establishments*100,
+    percent_within = percent_within*100,
+    sources = sources*100
+  )
+
+ggplot(kenosha, aes(x = year)) +
+  geom_line(aes(y = pop, color = "Population")) +
+  geom_line(aes(y = permits, color = "Housing Permits")) +
+  geom_line(aes(y = establishments, color = "Private Establishments")) +
+  geom_line(aes(y = sources, color = "Withdrawal Sources")) +
+  geom_vline(xintercept = c(2010, 2021), linetype = "dashed", color = "black") +
+  labs(title = "Kenosha County (2010, 2021)",
+       x = "Year",
+       y = "% Change") +
+  scale_color_manual(values = c(
+    "Population" = "blue",
+    "Housing Permits" = "red",
+    "Private Establishments" = "orange",
+    "Withdrawal Sources" = "green"
+  ), name = NULL) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5))
+
+#------------------------------------------------------------------------------#
+# waukesha ----
+
+waukesha = masterdata_changes |> 
+  filter(county == "Waukesha County") |> 
+  mutate(
+    pop = population*100,
+    permits = permits*100,
+    establishments = establishments*100,
+    percent_within = percent_within*100,
+    sources = sources*100
+  )
+
+ggplot(waukesha, aes(x = year)) +
+  geom_line(aes(y = pop, color = "Population")) +
+  geom_line(aes(y = permits, color = "Housing Permits")) +
+  geom_line(aes(y = establishments, color = "Private Establishments")) +
+  geom_line(aes(y = sources, color = "Withdrawal Sources")) +
+  geom_vline(xintercept = c(2006, 2010), linetype = "dashed", color = "black") +
+  labs(title = "Waukesha County (2006, 2010)",
+       x = "Year",
+       y = "% Change") +
+  scale_color_manual(values = c(
+    "Population" = "blue",
+    "Housing Permits" = "red",
+    "Private Establishments" = "orange",
+    "Withdrawal Sources" = "green"
+  ), name = NULL) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5))
+
+#------------------------------------------------------------------------------#
+# racine ----
+
+racine = masterdata_changes |> 
+  filter(county == "Racine County") |> 
+  mutate(
+    pop = population*100,
+    permits = permits*100,
+    establishments = establishments*100,
+    percent_within = percent_within*100,
+    sources = sources*100
+  )
+
+ggplot(racine, aes(x = year)) +
+  geom_line(aes(y = pop, color = "Population")) +
+  geom_line(aes(y = permits, color = "Housing Permits")) +
+  geom_line(aes(y = establishments, color = "Private Establishments")) +
+  geom_line(aes(y = sources, color = "Withdrawal Sources")) +
+  geom_vline(xintercept = 2018, linetype = "dashed", color = "black") +
+  labs(title = "Racine County (2018)",
+       x = "Year",
+       y = "% Change") +
+  scale_color_manual(values = c(
+    "Population" = "blue",
+    "Housing Permits" = "red",
+    "Private Establishments" = "orange",
+    "Withdrawal Sources" = "green"
+  ), name = NULL) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5))
